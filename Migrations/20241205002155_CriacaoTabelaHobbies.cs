4538f586cb2ab771d610af3a3106fa124606d2bd
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-12-04 21:54:35 -0300

    migrations

---
+++ Migrations/20241205002155_CriacaoTabelaHobbies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Testezin.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaHobbies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hobbies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Aniversario = table.Column<DateOnly>(type: "date", nullable: false),
+                    Hobbie = table.Column<string>(type: "text", nullable: true),
+                    Gostos = table.Column<string>(type: "text", nullable: true),
+                    PossiveisPresentes = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hobbies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hobbies");
+        }
+    }
+}
--- obj/Debug/net8.0/Testezin.AssemblyInfo.cs
+++ obj/Debug/net8.0/Testezin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testezin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d473afea352a2890ee7ad3872a057d6a1cf08b92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8f59a548b628fa65c050091f469aea386df06c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testezin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testezin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

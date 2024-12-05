04f0b457bda3c7c88c26de76120b17caeb7e1b90
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-12-05 00:19:16 -0300

    get por id

--- Controllers/HobbiesController.cs
+++ Controllers/HobbiesController.cs
@@ -24,5 +24,12 @@ namespace Testezin.Controllers
             contexto.SaveChanges();
             return Ok(hobbie);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterHobbie(int id){
+            var hobbie = contexto.Hobbies.Find(id);
+            if (hobbie == null) return NotFound();
+            return Ok(hobbie);
+        }
     }
 }

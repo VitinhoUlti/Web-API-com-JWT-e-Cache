a9113491da565d0855b6cca25ecdd24176c43fb9
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-12-30 02:54:59 -0300

    login e outras rotas de usuario

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -32,6 +32,23 @@ namespace Testezin.Controllers
             return CreatedAtAction(nameof(ObterId), new {id = usuario.Id, token = token}, usuario);
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(Usuarios usuario){
+            var usuarioCache = _memoryCache.Get(usuario.ToString());
+            if(_memoryCache.TryGetValue(usuario.ToString(), out usuarioCache)) {return Ok(usuarioCache);}
+
+            var login = from pessoa in contexto.Usuarios where pessoa.Nome.ToLower().Contains(usuario.Nome.ToLower()) && pessoa.Senha.Contains(usuario.Senha) select pessoa;
+            if(login == null) return NotFound();
+
+            var memorycacheoptions = new MemoryCacheEntryOptions {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                SlidingExpiration = TimeSpan.FromSeconds(1200)
+            };
+            _memoryCache.Set(usuario.ToString(), login, memorycacheoptions);
+            return Ok(usuario);
+        }
+
         [HttpGet("id/{id}")]
         [Authorize]
         public IActionResult ObterId(int id){
@@ -47,6 +64,31 @@ namespace Testezin.Controllers
             };
             _memoryCache.Set(id.ToString(), usuario, memorycacheoptions);
             return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public IActionResult AtualizarUsuarioporId(int id, Usuarios novoUsuario){
+            var usuario = contexto.Usuarios.Find(id);
+            if (usuario == null) return NotFound();
+
+            usuario.Nome = novoUsuario.Nome;
+            usuario.Senha = novoUsuario.Senha;
+
+            contexto.Usuarios.Update(usuario);
+            contexto.SaveChanges();
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public IActionResult DeletarUsuario(int id){
+            var hobbie = contexto.Usuarios.Find(id);
+            if (hobbie == null) return NotFound();
+
+            contexto.Usuarios.Remove(hobbie);
+            contexto.SaveChanges();
+            return NoContent();
         }
     }
 }

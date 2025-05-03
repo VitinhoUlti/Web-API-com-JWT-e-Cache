5d3d37e6cf088a4ce1deeefcc0707b11d8d800e5
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-05-03 03:30:59 -0300

    retirando o codigo do migrate
    Não há mais necessidade do codigo do migrate, a propria api já cria sua migration automaticamente

--- migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Instalando dotnet ef..."
-dotnet tool install --global dotnet-ef
-echo "Aplicando migrations..."
-dotnet ef database update --connection "Host=db;Port=5430;Database=${POSTGRES_DB};User ID=${POSTGRES_USER};Password=${POSTGRES_PASSWORD};" --startup-project /src/Testezin.csproj --project /src/Testezin.csproj
-echo "Migrations aplicadas."
--- obj/Debug/net8.0/Testezin.AssemblyInfo.cs
+++ obj/Debug/net8.0/Testezin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testezin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0052b1b653511f0a89eecf7a72625baf17155d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf8330154e2d04838fb0c5a63c945c2315ddb03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testezin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testezin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
